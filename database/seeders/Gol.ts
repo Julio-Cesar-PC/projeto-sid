214a36725ada649d2fc0010f8f5091edf9803b2e
lckrugel <lckrugel@inf.ufsm.br>
2023-11-06 10:49:27 -0300

    Adiciona todos os registros ao mesmo tempo para o banco

--- database/seeders/Gol.ts
+++ database/seeders/Gol.ts
@@ -6,14 +6,20 @@ import Gol from 'App/Models/Gol'
 
 export default class extends BaseSeeder {
   public async run () {
-    let gols = fs.readFileSync('goalscorers.csv', 'utf8')
-    let parsed = Papa.parse(gols, {header: true})
+    let golsDump = fs.readFileSync('goalscorers.csv', 'utf8')
+    let parsed = Papa.parse(golsDump, { header: true })
 
-    let golsArray = parsed.data
+    let parsedDataArray = parsed.data
 
-    for (let i = 0; i < golsArray.length; i++) {
-      const element = golsArray[i];
-      await Gol.create({
+    // Mantém todos os registros em memória e depois adiciona
+    // todos ao mesmo tempo para o banco
+    let golsArray: Gol[] = []
+    
+    for (let i = 0; i < parsedDataArray.length; i++) {
+      const element = parsedDataArray[i]
+      if (!element || element.date == '') break
+      const gol = new Gol()
+      gol.fill({
         data: element.date,
         time_casa: element.home_team,
         time_visitante: element.away_team,
@@ -22,7 +28,10 @@ export default class extends BaseSeeder {
         minuto: element.minute,
         contra: element.own_goal,
         penalti: element.penalty
-      })
+      })      
+      golsArray.push(gol)
     }
-  }
+
+    await Gol.createMany(golsArray)
+  }  
 }
